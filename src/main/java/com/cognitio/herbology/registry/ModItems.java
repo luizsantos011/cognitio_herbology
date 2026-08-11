4b4ea5dacf7d80fc23d0a3904df89cc211114f9d
luiz <luizsantost4@gmail.com>
2026-08-11 01:04:16 -0300

    feat(herbology): adicionar item Linfa Fantasma (Phantom Lymph)
    - Arquivo de pixel art importado para assets/cognitio_herbology/textures/item/belladonna/phantom_lymph.png
    - Registro básico do item em ModItems.java
    - Criado arquivo de modelo JSON para renderização
    - Chaves de tradução adicionadas em pt_br e en_us

--- src/main/java/com/cognitio/herbology/registry/ModItems.java
+++ src/main/java/com/cognitio/herbology/registry/ModItems.java
@@ -41,6 +41,8 @@ public class ModItems {
             
     public static final DeferredItem<Item> BELLADONNA = ITEMS.register("belladonna",
             () -> new ItemNameBlockItem(com.cognitio.herbology.registry.ModBlocks.BELLADONNA.get(), new Item.Properties()));
+
+    public static final DeferredItem<Item> PHANTOM_LYMPH = ITEMS.registerSimpleItem("phantom_lymph", new Item.Properties());
             
     public static final DeferredItem<Item> APOTHECARY_GRIMOIRE = ITEMS.registerSimpleItem("apothecary_grimoire", new Item.Properties().stacksTo(1));
 
